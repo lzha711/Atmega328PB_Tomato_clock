d13ed3eafb4db8a09759674650cb115960051605
Lily Zhang <52764277+lzha711@users.noreply.github.com>
2021-06-07 21:02:56 +1200

    add wording display and reset

--- down_counting.c
+++ down_counting.c
@@ -9,7 +9,7 @@
 #define F_CPU 16000000UL
 #define true 1
 #define false 0
-#define POV_delay 10 //15ms works fine too
+#define POV_delay 5 //15ms works fine too
 
 // library
 #include <avr/io.h>
@@ -42,10 +42,17 @@ uint8_t seven_seg_digits[10][7] = {
 	{ 1,1,1,1,0,1,1 }   // = 9
 };
 
+//a-g 7 segment values for each number
+uint8_t seven_seg_word[4][7] = {
+	{ 1,1,1,0,1,1,1 },  // = R
+	{ 1,0,0,1,1,1,1 },  // = E
+	{ 1,0,1,1,0,1,1 },  // = S
+	{ 1,0,0,0,1,1,0 },  // = T
+};
+
 void WriteDisplays(uint8_t digit, uint8_t displaynum){
 	uint8_t PD_pin; // initialize to PD2 (PD2 refers to 2)
 	PORTD &= 0b00000000; //clear port D
-	PORTB &= ~(1<<PB0); // clear PB0, dot pin
 	
 	// display select
 	if (displaynum == 0){
@@ -67,6 +74,32 @@ void WriteDisplays(uint8_t digit, uint8_t displaynum){
 	for (int segCount = 0; segCount <7; ++segCount){
 		PD_pin = segCount + 1;
 		PORTD |= (seven_seg_digits[digit][segCount]<<PD_pin); //assign [digit][a-g] to PD1-PD7
+	}
+}
+
+void WriteWords(uint8_t word, uint8_t displaynum){
+	uint8_t PD_pin; // initialize to PD2 (PD2 refers to 2)
+	PORTD &= 0b00000000; //clear port D
+	
+	// display select
+	if (displaynum == 0){
+		PORTB &= ~(1<<PB2); //select Sec2 display
+		PORTB |= (1<<PB3) | (1<<PB4) | (1<<PB5); //disable all other modules
+		}else if (displaynum == 1){
+		PORTB &= ~(1<<PB3); //select Sec1 display
+		PORTB |= (1<<PB2) | (1<<PB4) | (1<<PB5); //disable all other modules
+		}else if (displaynum == 2){
+		PORTB &= ~(1<<PB4); //select Min2
+		PORTB |= (1<<PB2) | (1<<PB3) | (1<<PB5); //disable all other modules
+		}else if (displaynum == 3){
+		PORTB &= ~(1<<PB5); //select Min1
+		PORTB |= (1<<PB2) | (1<<PB3) | (1<<PB4); //disable all other modules
+	}
+	
+	// assign display number
+	for (int segCount = 0; segCount <7; ++segCount){
+		PD_pin = segCount + 1;
+		PORTD |= (seven_seg_word[word][segCount]<<PD_pin); //assign [word][a-g] to PD1-PD7
 	}
 }
 
@@ -110,20 +143,31 @@ void CalculateMinutes(void){
 	    }
 }
 
+void Display_REST(void){
+	WriteWords(3,0); //T at sec2
+	_delay_ms(POV_delay); 
+	WriteWords(2,1); //S at sec1
+	_delay_ms(POV_delay);
+	WriteWords(1,2); //E at min2
+	_delay_ms(POV_delay);
+	WriteWords(0,3); //R at min1
+	_delay_ms(POV_delay);
+}
 
 void IO_init(void){
-	DDRD = 0b11111100; //set PD2-PD7 as output
+	DDRD = 0b11111110; //set PD1-PD7 as output
 	DDRB = 0b00111111; //set PB0-PB5 as output
 	DDRC &= ~(1<<PC1); //set PC1 as input
+	PORTC |= (1<<PC1); //use internal pull-up resistor
 }
 
 void TIMER1_init(void){
     TCCR1A = 0b00000000; //normal mode
 	TCCR1B = 0b00001101; //CTC mode, clk_t0 = clk_io/1024
 	OCR1A = 0x3D08; // trigger interrupt every 1s
-	// OCR1A = 0x01F4; //for quick testing
+	// OCR1A = 0x00F4; //for quick testing
 	TIMSK1 = 0b00000010; // set bit OCIE1A, timer/counter1 output compare match A interrupt enable
-	PORTB |= (1<<PB1); //set OC1A as output, doesn't matter intput or output
+	PORTB |= (1<<PB1); //set OC1A as output, doesn't matter input or output
 }
 
 
@@ -168,8 +212,9 @@ int main(void)
 			CalculateSeconds();
 			CalculateMinutes();
 		}else{
-			//display something else, like REST
-		}	
+			//display REST
+			Display_REST();
+		}
     }
 }
 
